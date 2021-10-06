b38aba5625e1c179fa885741772fa1a5ae4189e2
Andre Henrique dos Santos <andrehs@outlook.com.br>
2021-10-06 19:56:31 -0300

    Refatoração do autoload

--- autoload.php
+++ autoload.php
@@ -5,6 +5,7 @@ spl_autoload_register(
     {
         $namespace = str_replace("PortalDeNoticias\\", "", $namespace);
         $namespace = str_replace("\\", DIRECTORY_SEPARATOR, $namespace);
+        $namespace = str_replace("Controller", "controller", $namespace);
         $path = str_replace('public', 'source', getcwd()) . DIRECTORY_SEPARATOR . $namespace . '.php';
 
         include_once $path;
